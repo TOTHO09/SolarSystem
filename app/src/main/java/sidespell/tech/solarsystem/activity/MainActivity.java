3fb16f511d8e153fc83f2ad42ec35959e3c3a30b
TOTHO09 <martinodoroon@gmail.com>
2016-01-07 20:09:28 +0800

    first commit and final

--- app/src/main/java/sidespell/tech/solarsystem/activity/MainActivity.java
+++ app/src/main/java/sidespell/tech/solarsystem/activity/MainActivity.java
@@ -1,25 +1,47 @@
 package sidespell.tech.solarsystem.activity;
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 import sidespell.tech.solarsystem.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    Spinner spinner;
+    ArrayAdapter<CharSequence> adapter;
 
+    ImageView imageView;
+    private TypedArray planetsImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        planetsImage = getResources().obtainTypedArray(R.array.img_planets);
+        imageView = (ImageView)findViewById(R.id.imageView);
+        spinner = (Spinner)findViewById(R.id.planets_spinner);
+        adapter = ArrayAdapter.createFromResource(this,
+                R.array.planets_array,
+                android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapter);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                imageView.setImageResource(planetsImage.getResourceId(position,-1));
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
 }
